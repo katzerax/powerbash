8529a4d65ea16ef1e7fe2f528e17a1391f89eca5
K <65287118+katzerax@users.noreply.github.com>
2025-05-10 03:47:22 -0700

    swapped out deprecated  for 

--- modules/themes.sh
+++ modules/themes.sh
@@ -8,28 +8,28 @@ unseperated_powerline_dark1() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 235) $distro_logo  $(fc 247) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 235)\[\e[m\] " ;;
-		*) PS1="$(bc 235) $distro_logo  $(fc 247) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 235)\[\e[m\] " ;;
+		*) PS1="$(bc 235) $distro_logo  $(fc 247) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 235)\[\e[m\] " ;;
 	esac
 }
 unseperated_powerline_dark2() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 236) $distro_logo  $(fc 248) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 236)\[\e[m\] " ;;
-		*) PS1="$(bc 236) $distro_logo  $(fc 248) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 236)\[\e[m\] " ;;
+		*) PS1="$(bc 236) $distro_logo  $(fc 248) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 236)\[\e[m\] " ;;
 	esac
 }
 unseperated_powerline_dark3() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 237) $distro_logo  $(fc 249) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 237)\[\e[m\] " ;;
-		*) PS1="$(bc 237) $distro_logo  $(fc 249) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 237)\[\e[m\] " ;;
+		*) PS1="$(bc 237) $distro_logo  $(fc 249) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 237)\[\e[m\] " ;;
 	esac
 }
 unseperated_powerline_dark4() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 238) $distro_logo  $(fc 250) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 238)\[\e[m\] " ;;
-		*) PS1="$(bc 238) $distro_logo  $(fc 250) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 238)\[\e[m\] " ;;
+		*) PS1="$(bc 238) $distro_logo  $(fc 250) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 238)\[\e[m\] " ;;
 	esac
 }
 
@@ -66,28 +66,28 @@ seperated_powerline_dark1() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 252)$(fc 235) $distro_logo $(bc 235)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 235)\[\e[m\] " ;;
-		*) PS1="$(bc 252)$(fc 235) $distro_logo $(bc 236)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 235)\[\e[m\] " ;;
+		*) PS1="$(bc 252)$(fc 235) $distro_logo $(bc 236)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 235)\[\e[m\] " ;;
 	esac
 }
 seperated_powerline_dark2() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 252)$(fc 236) $distro_logo $(bc 236)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 236)\[\e[m\] " ;;
-		*) PS1="$(bc 252)$(fc 236) $distro_logo $(bc 236)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err\\[\\e[0m\\]$(fc 236)\[\e[m\] " ;;
+		*) PS1="$(bc 252)$(fc 236) $distro_logo $(bc 236)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err\\[\\e[0m\\]$(fc 236)\[\e[m\] " ;;
 	esac
 }
 seperated_powerline_dark3() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 252)$(fc 237) $distro_logo $(bc 237)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 237)\[\e[m\] " ;;
-		*) PS1="$(bc 252)$(fc 237) $distro_logo $(bc 237)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 237)\[\e[m\] " ;;
+		*) PS1="$(bc 252)$(fc 237) $distro_logo $(bc 237)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 237)\[\e[m\] " ;;
 	esac
 }
 seperated_powerline_dark4() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 252)$(fc 238) $distro_logo $(bc 238)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 238)\[\e[m\] " ;;
-		*) PS1="$(bc 252)$(fc 238) $distro_logo $(bc 238)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 238)\[\e[m\] " ;;
+		*) PS1="$(bc 252)$(fc 238) $distro_logo $(bc 238)$(fc 252) $(fc 2)\w$(fc 6) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 238)\[\e[m\] " ;;
 	esac
 }
 nologo_seperated_powerline_dark1() {
@@ -123,7 +123,7 @@ colors_powerline() {
 	err=$?
 	case $err in
 		0) PS1="$(bc 214)$(fc 235) $distro_logo $(bc 24)$(fc 214) $(fc 2)\w$(fc 220) $(__git_ps1) $(fc 34)✓ \\[\\e[0m\\]$(fc 24)\[\e[m\] " ;;
-		*) PS1="$(bc 214)$(fc 235) $distro_logo $(bc 24)$(fc 214) $(fc 2)\w$(fc 220) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 24)\[\e[m\] " ;;
+		*) PS1="$(bc 214)$(fc 235) $distro_logo $(bc 24)$(fc 214) $(fc 2)\w$(fc 220) $(__git_ps1) $(fc 1) $err \\[\\e[0m\\]$(fc 24)\[\e[m\] " ;;
 	esac
 
 }
